b5ac28618c19da79e80fe97e89fb01ed7bc6ab07
rituraj735 <rituraj735@gmail.com>
2020-11-04 11:02:58 +0530

    app.js modified

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use('/research', research);
 app.use('/training/conducted', trainingConducted);
 app.use('/training/attended', trainingAttended);
 app.use('/subject', subject);
-app.use('/', passport.authenticate('jwt', { session: false }), securedRoutes);
+// app.use('/', passport.authenticate('jwt', { session: false }), securedRoutes);
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
