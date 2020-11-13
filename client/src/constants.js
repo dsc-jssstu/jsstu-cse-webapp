633561ada0a3105516e35da85680b48d1a2ef5da
Alaap Surendran <alaapsurendran@gmail.com>
2020-11-13 15:51:36 +0530

    client: split apiBaseUrl into baseUrl and apiBaseUrl

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,3 @@
-//export const apiBaseUrl = "http://localhost:4000/api";
-export const apiBaseUrl = "http://cse-jssstu.herokuapp.com/api";
+// export const baseUrl = "http://localhost:4000";
+export const baseUrl = "http://cse-jssstu.herokuapp.com";
+export const apiBaseUrl = baseUrl + "/api";
